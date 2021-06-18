5754fb2abd1f53eae777f9d07f5dfc826ff28013
Steve Kerrison <steve.kerrison@jcu.edu.au>
2021-06-18 17:28:55 +0800

    Refactor and improve trace animation

--- data/simple_network/simple.js
+++ data/simple_network/simple.js
@@ -19,6 +19,24 @@ class DabaoActivity {
     }
 }
 
+class DabaoARP extends DabaoActivity {
+    constructor ({
+        cy,
+        querier,
+        responder,
+        address
+    } = {}) {
+        super({cy: cy, initiator: querier, sender: querier, receiver: responder})
+        this.address = address
+    }
+
+    *trace() {
+        if (this.cy.data('lod').has('arp')) {
+            throw "ARP not implemented yet"
+        }
+    }
+}
+
 /**
  * MAC (L2) packet generation.
  */
@@ -49,15 +67,21 @@ class DabaoMAC extends DabaoActivity {
             let cur;
             for (node of path) {
                 if (node == this.sender) {
-                    console.log("Skipping self")
                     cur = node
                     continue
                 }
-                if (this.cy.data('lod').has('arp')) {
-                    throw "ARP not implemented yet"
+                let arp = new DabaoARP({
+                    cy: this.cy,
+                    querier: this.sender,
+                    responder: this.receiver,
+                    address: 'TODO'
+                })
+                for (let packet of arp.trace()) {
+                    yield packet
                 }
                 yield {
                     "_schema_type": "ethernet_l2",
+                    "_path": [cur, node],
                     "source_mac": cur.id(),
                     "destination_mac": node.id(),
                     "_payload": this.packet
@@ -472,174 +496,92 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-  }
-
-/**
- * Simulate an HTTP request
- * 
- * Assumes a route can exist between client and server and that
- * the specified ports would work.
- * 
- * @param client The Cytoscape node that will originate the request
- * @param server The Cytoscape node that will provide the response
- * @param request Content of the request (not checked for correctness)
- * @param response Content of the response (not checked for correctness)
- * @returns A trace of packets that represent this request. Lower-level simulators may be able to expand this trace.
- */
-var simulate_http = function(client, server, request, response) {
-    /* Loosely parse the HTTP request and get the headers out, mainly to find the host */
-    var headers = new Map();
-    for (var line of request.split('\n\n')[0].split('\n').slice(1)) {
-        var separator = line.indexOf(': ');
-        var field = line.slice(0, separator);
-        var value = line.slice(separator+2);
-        headers.set(field.toLowerCase(), value);
-    }
-    trace = {
-        "assumptions": [
-            /* We assume that DNS would normally be performed to resolve the server's IP from the given host */
-            {'dns': headers.get('host')},
-            /* We use TCP to carry HTTP/1.1 traffic */
-            {'tcp': {'destination': 80}},
-            /* We use IP, ARP, etc to make the above work, but they can be simulated by other layers */
-        ],
-        "entries": [
-            {
-                "path": [client.id(), server.id()],
-                "packet": {
-                    "_schema_type": "application",
-                    "_raw": request.split('').map((c) => { return c.charCodeAt(0); }),
-                    "_payload": {
-                        "_schema_type": "descriptive_field",
-                        "name": "HTTP v1.1 request",
-                        "value": request,
-                        "description": `A request being sent to whoever is the registered host of "${headers.get('host')}"`
-                    }
-                }
-            },
-            {
-                "path": [client.id(), server.id()],
-                "packet": {
-                    "_schema_type": "application",
-                    "_raw": request.split('').map((c) => { return c.charCodeAt(0); }),
-                    "_payload": {
-                        "_schema_type": "descriptive_field",
-                        "name": "HTTP v1.1 response (200 OK)",
-                        "value": response,
-                        "description": `A successful response, containing a simple HTML document`
-                    }
-                }
-            }
-        ]
-    }
-    return trace;
-};
+}
 
 class dabao_packet_animation {
-    constructor(packetid, path, auto = true) {
+    constructor(cy, packetid, trace, auto = true) {
         this.packetid = packetid;
-        this.path = path;
-        this.position = 0;
+        this.trace = trace
         this.auto = auto;
+        this.packet = undefined
+        this.remote = false
+        this.itv = this.trace.next()
+        this.cy = cy
+        this.in_network = false
     }
 
     animate() {
         var nextfunc = this.auto ? this.animate.bind(this) : undefined;
-        if (this.position == 0) {
-            document.getElementById("next").disabled = this.auto;
-            var cy = window.cy;
-            var curpos = this.path[this.position].position();
-            this.packet = cy.add({
+        if (this.remove) {
+            console.log("Removing packet node")
+            this.packet.remove()
+            // Done!
+            return;
+        }
+        if (!this.packet) {
+            let segment = this.itv.value['_path']
+            document.getElementById("next").disabled = this.auto
+            var curpos = segment[0].position()
+            console.log("Creating node and easing in");
+            this.packet = this.cy.add({
                 'group': 'nodes',
                 'data': {'id': 'packet', 'label': 'Packet'},
                 'position': {x: curpos.x + 20, y: curpos.y + 40},
                 'complete': nextfunc
             });
-            this.position += 1;
             this.packet.animate({
                 'duration': 500,
                 'style': { 'opacity': 1.0 },
                 'easing': 'ease-in',
                 'complete': nextfunc
             });
-        } else if (this.position == this.path.size()) {
-            this.position += 1;
+            // Early out
+            return
+        }
+        if (this.itv.done) {
+            console.log("Ending animation and easing out")
             this.packet.animate({
                 'duration': 500,
                 'style': { 'opacity': 0 },
                 'easing': 'ease-out',
                 'complete': nextfunc
-            });
-        } else if (this.position > this.path.size()) {
-            this.packet.remove();
-        } else {
-            var curnode = this.path[this.position-1];
-            var nextnode = this.path[this.position];
-            var nextpos = nextnode.position();
-            var ease = 'linear';
-            if (this.position == 0) {
-                ease = 'ease-in-sine';
-            } else if (this.position == this.path.size() - 1) {
-                ease = 'ease-out-sine';
-            }
-            this.position += 1;
-            // TODO: animate selection properly
-            curnode.edgesTo(nextnode).select();
-            curnode.select();
-            nextnode.select();
-            this.packet.animate({
-                'duration': 500,
-                'position': { x: nextpos.x + 20, y: nextpos.y + 40 },
-                'easing': ease,
-                'style': { 'opacity': 1.0 },
-                'complete': nextfunc
-            });
-            // TODO: Animate selection properly
-            curnode.unselect();
-            curnode.edgesTo(nextnode).unselect();
+            })
+            this.remove = true
+            return
+        }
+        let curnode = this.itv.value['_path'][0];
+        let nextnode = this.itv.value['_path'][1];
+        let vizpath = this.cy.data('fw').path(curnode, nextnode).nodes()
+        switch (vizpath.size()) {
+            case 2:
+                // Nothing to do
+                break
+            case 3:
+                if (this.in_network) {
+                    curnode = vizpath[1]
+                    this.in_network = false
+                } else {
+                    curnode.edgesTo(nextnode).select()
+                    vizpath.select()
+                    nextnode = vizpath[1]
+                    this.in_network = true
+                }
+                break
+            default:
+                throw "Unexpected visualisation path length"
+        }
+        var nextpos = nextnode.position()
+        var ease = 'linear';
+        this.packet.animate({
+            'duration': 500,
+            'position': { x: nextpos.x + 20, y: nextpos.y + 40 },
+            'easing': ease,
+            'style': { 'opacity': 1.0 },
+            'complete': nextfunc
+        });
+        if (!this.in_network) {
+            this.itv = this.trace.next()
         }
         return;
     }
 }
-
-var trace = {
-    "entries": [
-        {"$comment": "We define a single-packet HTTP request/response here, then use some simple simulation to expand out the trace"},
-        {
-            "path": ["client", "web"],
-            "packet": {
-                "_schema_type": "ip",
-                "version": {
-                    "_schema_type": "descriptive_field",
-                    "value": 4
-                },
-                "_payload": {
-                    "_schema_type": "ipv4",
-                    "source_ip_address": {
-                        "_schema_type": "descriptive_field",
-                        "value": "10.100.0.100"
-                    },
-                    "destination_ip_address": {
-                        "_schema_type": "descriptive_field",
-                        "value": "192.168.111.20"
-                    },
-                    "protocol": {
-                        "_schema_type": "descriptive_field",
-                        "value": "TCP"
-                    },
-                    "_payload": {
-                        "_schema_type": "TCP",
-                        "destination_port": {
-                            "_schema_type": "descriptive_field",
-                            "value": 80
-                        },
-                        "_payload": {
-                            "_schema_type": "descriptive_field",
-                            "value": "GET / HTTP/1.1\nHost: example.org\n\n"
-                        }
-                    }
-                }
-            }
-        }
-    ]
-};
